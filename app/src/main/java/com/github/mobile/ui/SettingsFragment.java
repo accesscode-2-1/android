21ba6a11ca67033633cb1d2d24d5d4b18b65449b
m47bell <m.vidals047@gmail.com>
2015-06-20 09:17:59 -0400

    adding settings fragment

--- app/src/main/java/com/github/mobile/ui/SettingsFragment.java
+++ app/src/main/java/com/github/mobile/ui/SettingsFragment.java
@@ -1,10 +1,20 @@
 package com.github.mobile.ui;
 
+import android.os.Bundle;
+
 /**
  * Created by c4q-marbella on 6/19/15.
  */
 public class SettingsFragment extends android.support.v4.app.Fragment {
 
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+    }
 
 
 
